a8793c23617238614123412253d3d6b058d151a1
Kendall Swanson <42819374+kvs1995@users.noreply.github.com>
2022-03-29 00:22:15 -0500

    added in base for the navigation menu

--- src/components/menu/NavMenu.js
+++ src/components/menu/NavMenu.js
@@ -1,6 +1,10 @@
 import './navmenu.css';
-import { LineOutlined, MenuOutlined, MoreOutlined } from 'antd';
-import { Sider, useState } from 'react';
+//update the MehOutlined
+import { useState } from 'react';
+import {  SmileOutlined, ProfileOutlined, CodeOutlined, PhoneOutlined, LineOutlined, MenuOutlined, MoreOutlined } from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+
+const { Header, Sider, Content } = Layout;
 
 export default function NavMenu() {
 
@@ -8,6 +12,23 @@ export default function NavMenu() {
 
   return(
     <>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo" />
+        <Menu theme="light" model="inline" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1" icon={<SmileOutlined />}>
+            Home
+          </Menu.Item>
+          <Menu.Item key="2" icon={<ProfileOutlined />}>
+            About Me
+          </Menu.Item>
+          <Menu.Item key="3" icon={<CodeOutlined />}> 
+            Portfolio
+          </Menu.Item>
+          <Menu.Item key="4" icon={<PhoneOutlined />}>
+            Contact Me
+          </Menu.Item>
+        </Menu>
+      </Sider>
     </>
   )
 }
