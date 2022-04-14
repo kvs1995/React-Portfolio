023f16888a80ec779912b4774c9126a394138f25
Kendall Swanson <42819374+kvs1995@users.noreply.github.com>
2022-04-14 01:59:26 -0500

    home page with name and title. styling

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,8 +3,11 @@ import './home.css';
 
 export default function Home() {
   return(
-    <>
-      <div> Hello</div>
-    </>
+
+    <div className="custom-body">
+        <h1 className="name">Kendall Swanson</h1>
+        <h3 className="title">Full-Stack Web Development</h3>
+    </div>
+
   )
 }
