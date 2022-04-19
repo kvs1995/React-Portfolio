fbc36068b36df9fe7ec21eae0ca0d3076e775a17
Kendall Swanson <42819374+kvs1995@users.noreply.github.com>
2022-04-19 01:53:17 -0500

    rearrange the contact ingo

--- src/components/contactme/ContactMe.js
+++ src/components/contactme/ContactMe.js
@@ -8,9 +8,9 @@ export default function ContactMe() {
         <h3 className="contact-title">Full-Stack Web Development</h3>
       </div>
       <div className="contact-links">
-        <a href="https://github.com/kvs1995" target="_blank"><GithubOutlined/> Github </a> 
-        <a href="mailto:kvscoding@gmail.com" target="_blank"><MailOutlined /> Shoot me an email!</a>
         <p><PhoneOutlined /> 312.636.9833</p>
+        <a href="mailto:kvscoding@gmail.com" target="_blank"><MailOutlined /> Shoot me an email!</a>
+        <a href="https://github.com/kvs1995" target="_blank"><GithubOutlined/> Github </a> 
         <a href="https://www.linkedin.com/in/kendallvswanson/" target="_blank"><LinkedinOutlined /> LinkedIn</a>
       </div>
     </div>
